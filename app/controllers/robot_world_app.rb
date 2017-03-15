ebcb85385066326a3c2c56f428306da55bb1ddce
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-15 11:19:51 -0600

    added new robot functionality

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,29 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    erb :dashboard
+  end
+
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
+
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
 end
 
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,42 @@
 require 'sqlite3'
 
 class Robot
+
+  attr_reader :name,
+              :city,
+              :state,
+              :department,
+              :id
+
   def initialize(robot_params)
-    @database = SQLite3::Database.new('db/robot_world_development.db')
+    @name = robot_params["name"]
+    @city = robot_params["city"]
+    @state = robot_params["state"]
+    @department = robot_params["department"]
+    @database = SQLite3::Database.new('db/robot_development.db')
     @database.results_as_hash = true
+    @id = robot_params["id"] if robot_params["id"]
   end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots")
+    robots.map do |robot|
+      Robot.new(robot)
+    end
+  end
+
+  def self.find
+    
+  end
+
 end
