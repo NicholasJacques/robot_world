bcaaddb077163ed3cb4913c5b88f6a3784819ef0
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-15 19:18:18 -0600

    completed baseline project

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -25,6 +26,21 @@ class RobotWorldApp < Sinatra::Base
   get '/robots/:id' do
     @robot = Robot.find(params[:id])
     erb :show
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+  delete '/robots/:id' do |id|
+    Robot.destroy(id.to_i)
+    redirect '/robots'
   end
 
 end
